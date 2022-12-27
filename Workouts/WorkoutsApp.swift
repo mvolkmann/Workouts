8791166db94123f786c919a4c056e69fe0b2e4dd
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-12-27 10:09:33 -0600

    Initial Commit

---
+++ Workouts/WorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutsApp.swift
+//  Workouts
+//
+//  Created by Mark Volkmann on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
