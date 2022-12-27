39f90ca6da70cce3b0c4d5fc9f6cf92d1b7c3aea
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-12-27 15:05:14 -0600

    styling

--- Workouts/ContentView.swift
+++ Workouts/ContentView.swift
@@ -37,25 +37,40 @@ struct ContentView: View {
     }
 
     private func labelledValue(_ label: String, _ value: Double) -> some View {
-        Text("\(label): \(String(format: "%.0f", value))")
+        HStack {
+            Text("\(label):")
+            Spacer()
+            Text(String(format: "%.0f", value))
+                .fontWeight(.bold)
+        }
+        .frame(maxWidth: 270)
     }
 
     var body: some View {
         VStack {
-            Text("Health Statistics for Past 7 Days")
+            Text("Health Statistics\nfor the Past 7 Days")
                 .font(.title)
-            labelledValue("Average Heart Rate", viewModel.heartRate)
-            labelledValue(
-                "Average Resting Heart Rate",
-                viewModel.restingHeartRate
-            )
-            labelledValue("Total Steps", viewModel.steps)
-            labelledValue("Total Calories Burned", viewModel.activeEnergyBurned)
+                .multilineTextAlignment(.center)
+                .padding(.bottom)
+            VStack(alignment: .leading) {
+                labelledValue("Average Heart Rate", viewModel.heartRate)
+                labelledValue(
+                    "Average Resting Heart Rate",
+                    viewModel.restingHeartRate
+                )
+                labelledValue("Total Steps", viewModel.steps)
+                labelledValue(
+                    "Total Calories Burned",
+                    viewModel.activeEnergyBurned
+                )
+            }
 
-            Text("Cycling Workout")
-                .font(.title)
-                .padding(.top)
+            Spacer()
+
             VStack {
+                Text("Cycling Workout")
+                    .font(.title)
+                    .padding(.top)
                 TextField("Cycling Miles", text: $cyclingMiles)
                     .focused($focusedField, equals: .cyclingMiles)
                     .numbersOnly($cyclingMiles, float: true)
@@ -70,13 +85,13 @@ struct ContentView: View {
                 .buttonStyle(.borderedProminent)
             }
         }
+        .padding()
         .alert(
             "Success",
             isPresented: $isShowingAlert,
             actions: {},
             message: { Text(message) }
         )
-
         .toolbar {
             ToolbarItem(placement: .keyboard) {
                 Button {
